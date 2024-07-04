5e301b0a3afb68c32eb0fd4953cbcee192355d3a
Cyril26 <cyrilodm26@gmail.com>
2024-07-04 08:03:06 +0000

    initial setup

---
+++ web/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,vue}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backdropBlur: ['hover', 'focus'],
+      backgroundColor: ['active'],
+      textColor: ['active'],
+    },
+  },
+  plugins: [],
+}
+
