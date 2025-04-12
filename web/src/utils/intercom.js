b758ee293780d1b1ef087a839168640f096aefff
Cyril26 <cyrilodm26@gmail.com>
2025-04-12 14:44:40 +0000

    intercom config

--- web/src/utils/intercom.js
+++ web/src/utils/intercom.js
@@ -1,9 +1,17 @@
 const APP_ID = import.meta.env.VITE_INTERCOM_APP_ID;
 
 export const loadIntercom = () => {
-  window.intercomSettings = {
-    app_id: APP_ID,
-  };
+  const storedUser = localStorage.getItem("user");
+  const intercomSettings = { app_id: APP_ID };
+
+  if (storedUser) {
+    const { user_id, first_name, last_name, email } = JSON.parse(storedUser);
+    intercomSettings.name = `${first_name} ${last_name}`;
+    intercomSettings.email = email;
+    intercomSettings.user_id = user_id;
+  }
+
+  window.intercomSettings = intercomSettings;
 
   (function () {
     var w = window;
